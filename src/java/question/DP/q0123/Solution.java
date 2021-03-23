d2dadb259cb603bf44b6c76adf8bb82cf79c379b
ye.qin <ye.qin@china.zhaogang.com>
2021-03-23 15:01:44 +0800

    commit

--- src/java/question/DP/q0123/Solution.java
+++ src/java/question/DP/q0123/Solution.java
@@ -21,7 +21,11 @@ public class Solution {
         dp[0][3] = -prices[0];
         dp[0][4] = 0;
         for (int i = 1; i < length; i++) {
-
+            dp[i][0] = dp[i - 1][0];
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] + prices[i]);
+            dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] - prices[i]);
+            dp[i][4] = Math.max(dp[i - 1][4], dp[i - 1][3] + prices[i]);
         }
         return dp[length - 1][4];
     }
