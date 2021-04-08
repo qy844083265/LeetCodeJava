764ce117573737dae492bff0fa15b5e27f858027
ribbonsalmark <qy844083265@163.com>
2021-04-08 21:35:50 +0800

    commit

---
+++ src/java/question/DP/q0714/Solution.java
@@ -0,0 +1,19 @@
+package java.question.DP.q0714;
+
+/**
+ * 你可以无限次地完成交易，但是你每笔交易都需要付手续费。如果你已经购买了一个股票，在卖出它之前你就不能再继续购买股票了。
+ * <p>
+ * 返回获得利润的最大值。
+ */
+public class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        dp[0][0] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[0] - fee);
+        }
+        return Math.max(dp[n - 1][1], dp[n - 1][0]);
+    }
+}
