55b400d0931da5190d7fa5615592452f6890c0ee
ribbonsalmark <qy844083265@163.com>
2021-04-08 22:32:51 +0800

    commit

---
+++ src/java/question/DP/q0309/Solution.java
@@ -0,0 +1,25 @@
+package java.question.DP.q0309;
+
+/**
+ * 设计一个算法计算出最大利润。在满足以下约束条件下，你可以尽可能地完成更多的交易（多次买卖一支股票）:
+ * <p>
+ * 你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ * 卖出股票后，你无法在第二天买入股票 (即冷冻期为 1 天)。
+ */
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[][] dp = new int[n][4];
+        dp[0][0] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], Math.max(dp[i - 1][3], dp[i - 1][1] - prices[i]));
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][3]);
+            dp[i][2] = dp[i - 1][0] + prices[i];
+            dp[i][3] = dp[i - 1][2];
+        }
+        return Math.max(dp[n - 1][3], Math.max(dp[n - 1][1], dp[n - 1][2]));
+    }
+}
