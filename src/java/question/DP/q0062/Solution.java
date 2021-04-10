cb15ac2c59d9bd2c2ecaf494920008b961f2a659
ribbonsalmark <qy844083265@163.com>
2021-04-10 10:29:23 +0800

    commit

--- src/java/question/DP/q0062/Solution.java
+++ src/java/question/DP/q0062/Solution.java
@@ -6,9 +6,6 @@ package java.question.DP.q0062;
 //
 //问总共有多少条不同的路径？
 //
-//来源：力扣（LeetCode）
-//链接：https://leetcode-cn.com/problems/unique-paths
-//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 public class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
