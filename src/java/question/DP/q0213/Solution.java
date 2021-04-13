9851c1f8a31feefd55c6bf481bd64f4b6bbe7e50
ribbonsalmark <qy844083265@163.com>
2021-04-13 21:12:44 +0800

    commit

---
+++ src/java/question/DP/q0213/Solution.java
@@ -0,0 +1,37 @@
+package java.question.DP.q0213;
+
+/**
+ * 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈
+ * ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
+ * 
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，能够偷窃到的最高金额。
+ * 
+ * @author ribbonsalmark
+ *
+ */
+public class Solution {
+	public int rob(int[] nums) {
+		if (nums.length == 0) {
+			return 0;
+		}
+		if (nums.length == 1) {
+			return nums[0];
+		}
+		int result1 = robRange(nums, 0, nums.length - 2);
+		int result2 = robRange(nums, 1, nums.length - 1);
+		return Math.max(result1, result2);
+	}
+
+	public int robRange(int[] nums, int start, int end) {
+		if (start == end) {
+			return nums[start];
+		}
+		int[] dp = new int[nums.length];
+		dp[start] = nums[start];
+		dp[start + 1] = Math.max(nums[start], nums[start + 1]);
+		for (int i = start + 2; i <= end; i++) {
+			dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+		}
+		return dp[end];
+	}
+}
---
+++ src/java/question/DP/q0718/Solution.java
@@ -0,0 +1,11 @@
+package java.question.DP.q0718;
+/**
+ * 给两个整数数组 A 和 B ，返回两个数组中公共的、长度最长的子数组的长度。
+ * @author ribbonsalmark
+ *
+ */
+public class Solution {
+    public int findLength(int[] A, int[] B) {
+
+    }
+}
