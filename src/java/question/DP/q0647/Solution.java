08e832920839ddb8a7465e480667f1e521c591c8
ribbonsalmark <qy844083265@163.com>
2021-04-22 00:26:59 +0800

    commit

---
+++ src/java/question/DP/q0647/Solution.java
@@ -0,0 +1,28 @@
+package java.question.DP.q0647;
+
+/**
+ * 给定一个字符串，你的任务是计算这个字符串中有多少个回文子串。
+ * <p>
+ * 具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
+ */
+public class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int result = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (j - i <= 1) {
+                        result++;
+                        dp[i][j] = true;
+                    } else if (dp[i + 1][j - 1]) {
+                        result++;
+                        dp[i][j] = true;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
