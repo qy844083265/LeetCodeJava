311f11c53116b839082fbf6964ce9dbbce23efc7
ribbonsalmark <qy844083265@163.com>
2021-04-23 23:01:26 +0800

    commit

---
+++ src/java/question/DP/q0516/Solution.java
@@ -0,0 +1,24 @@
+package java.question.DP.q0516;
+
+/**
+ * 给定一个字符串 s ，找到其中最长的回文子序列，并返回该序列的长度。可以假设 s 的最大长度为 1000 。
+ */
+public class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i + 1][j]);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
