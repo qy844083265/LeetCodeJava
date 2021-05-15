d47b82d0bd3027373415de454c01eab385481317
ribbonsalmark <qy844083265@163.com>
2021-05-15 23:47:05 +0800

    commit

--- src/java/question/Tree/q0404/Solution.java
+++ src/java/question/Tree/q0404/Solution.java
@@ -1,6 +1,7 @@
 package java.question.Tree.q0404;
 
 import java.common.TreeNode;
+import java.util.Stack;
 
 /**
  * 计算给定二叉树的所有左叶子之和。
@@ -24,5 +25,27 @@ public class Solution {
 			mid = root.left.val;
 		}
 		return mid + left + right;
+    }
+	
+	public int sumOfLeftLeaves2(TreeNode root) {
+		if(root == null) {
+			return 0;
+		}
+		Stack<TreeNode> stack = new Stack<>();
+		int result = 0;
+		stack.push(root);
+		while(!stack.isEmpty()) {
+			TreeNode node = stack.pop();
+			if(node.left != null && node.left.left == null && node.left.right == null) {
+				result += node.left.val;
+			}
+			if(node.right != null) {
+				stack.push(node.right);
+			}
+			if(node.left != null) {
+				stack.push(node.left);
+			}
+		}
+		return result;
     }
 }
